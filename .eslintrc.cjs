acc9de10d3636a9cb9c20aa08d39306fd6e75563
Mayank Verma <errmayank@gmail.com>
2023-09-07 01:14:13 +0530

    [feat] export superslug() to generate random slugs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,19 +4,31 @@ module.exports = {
     node: true,
     es2022: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  ignorePatterns: ['.eslintrc.cjs'],
   rules: {
-    '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    'prettier/prettier': 'warn',
   },
 };
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,7 +1,53 @@
-import { superslug } from '$src/main.js';
-import { expect, it } from 'vitest';
+import { expect, expectTypeOf, it } from 'vitest';
 
-it('superslug result', () => {
-  const result = superslug();
-  expect(result).toBe('helo-this-is-superslug');
+import { adjectives } from '$src/lib/adjectives.js';
+import { nouns } from '$src/lib/nouns.js';
+import { verbs } from '$src/lib/verbs.js';
+import { randomInteger, randomAdjective, randomNoun, randomVerb } from '$src/util/main.js';
+
+it('stress test randomInteger()', () => {
+  let min = 0;
+  let max = 100_000_000;
+
+  for (let i = 0; i < 100_000; i++) {
+    const integer = randomInteger(min, max);
+
+    expectTypeOf(integer).toBeNumber();
+    expect(integer).toEqual(parseInt(String(integer)));
+
+    min = randomInteger(min, max);
+    max = randomInteger(min, max);
+
+    if (max - min < 10) {
+      min = 0;
+      max = 100_000_000;
+    }
+  }
+});
+
+it('stress test randomAdjective()', () => {
+  for (let i = 0; i < 100_000; i++) {
+    const adjective = randomAdjective();
+
+    expectTypeOf(adjective).toBeString();
+    expect(adjectives).toContainEqual(adjective);
+  }
+});
+
+it('stress test randomNoun()', () => {
+  for (let i = 0; i < 100_000; i++) {
+    const noun = randomNoun();
+
+    expectTypeOf(noun).toBeString();
+    expect(nouns).toContainEqual(noun);
+  }
+});
+
+it('stress test randomVerb()', () => {
+  for (let i = 0; i < 100_000; i++) {
+    const verb = randomVerb();
+
+    expectTypeOf(verb).toBeString();
+    expect(verbs).toContainEqual(verb);
+  }
 });
