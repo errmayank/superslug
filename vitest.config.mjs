372f1fc7732ab9376c0507cc59555b6d7bb28cd9
Mayank Verma <errmayank@gmail.com>
2023-09-07 14:45:14 +0530

    update vitest coverage config

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -7,5 +7,9 @@ export default defineConfig({
     alias: {
       '$src/': `${cwd()}/src/`,
     },
+    coverage: {
+      provider: 'v8',
+      reporter: ['json', 'lcov', 'text', 'clover', 'text-summary'],
+    },
   },
 });
