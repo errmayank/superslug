be7e2214aba9b7cfd9cb8ee793006b2e0d3c39f6
Mayank Verma <errmayank@gmail.com>
2023-09-11 22:50:13 +0530

    [feat] ability to customize word case (#6)

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ![npm](https://img.shields.io/npm/v/superslug) [![codecov](https://codecov.io/gh/errmayank/superslug/graph/badge.svg?token=828D94V216)](https://codecov.io/gh/errmayank/superslug) ![GitHub](https://img.shields.io/github/license/errmayank/superslug) ![npm](https://img.shields.io/npm/dm/superslug)
 
-In case you're looking to create human readable identifiers, unique\* URLs, or just random strings; superslug can fill your slug generation needs.
+In case you're looking to create unique\* human readable identifiers, URLs, or just random strings; superslug can fill your slug generation needs.
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { randomAdjective, randomNoun, randomVerb } from '$src/util/main.js';
 
-export type SuperslugConfig = {
+export type Format =
+  | 'UPPERCASE'
+  | 'lowercase'
+  | 'Startcase'
+  | 'endcasE'
+  | 'AlTeRnAtEcAsE'
+  | 'aLtErNaTeCaSe';
+
+export type Config = {
   separator?: ('-' | '_' | '.') | {};
+  format?: Format;
 };
 
-export const superslug = (config?: SuperslugConfig) => {
-  const separator = config?.separator ?? '-';
+export const superslug = (config?: Config) => {
+  const spt = config?.separator ?? '-';
+  const fmt = config?.format ?? 'lowercase';
 
-  return `${randomAdjective()}${separator}${randomNoun()}${separator}${randomVerb()}`;
+  return `${randomAdjective(fmt)}${spt}${randomNoun(fmt)}${spt}${randomVerb(fmt)}`;
 };
